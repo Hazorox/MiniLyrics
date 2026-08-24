c122b2e89fc61a15daf5407c063d27573f4076bd
Hamza Awad <hamzaawad8899@gmail.com>
2026-08-24 18:06:58 +0300

    added base window options

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
-use eframe::{NativeOptions, egui};
+use eframe::{NativeOptions, egui::{self, IconData, Vec2b}};
 
-fn main(){
-    let native_options = NativeOptions::default();
-    let _ = eframe::run_native("My Lyrics",native_options, Box::new(|cc| Ok(Box::new(App::new(cc)))));
+fn main() {
+    let native_options = NativeOptions {
+        viewport: eframe::egui::ViewportBuilder::default()
+        .with_title("Mini Lyrics")
+        .with_always_on_top()
+        .with_min_inner_size(egui::Vec2::new(320.0,216.0))
+        .with_inner_size(egui::Vec2::new(480.0,270.0))
+        .with_title_shown(false)
+        .with_titlebar_shown(false)
+        .with_active(false)
+        .with_title_shown(false)
+        .with_titlebar_buttons_shown(false)
+        .with_decorations(false)
+        .with_has_shadow(false)
+        .with_icon(IconData::n)
+        ,
+        ..Default::default()
+    };
+    let _ = eframe::run_native(
+        "My Lyrics",
+        native_options,
+        Box::new(|cc| Ok(Box::new(App::new(cc)))),
+    );
 }
 
 #[derive(Default)]
-struct App{} 
+struct App {}
 
 impl App {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_global_style.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
@@ -17,9 +37,9 @@ impl App {
         Self::default()
     }
 }
-impl eframe::App for App{
-    fn ui(&mut self, ui : &mut egui::Ui, _frame: &mut eframe::Frame){
-        egui::CentralPanel::default().show(ui,|ui|{
+impl eframe::App for App {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ui, |ui| {
             ui.label("test");
         });
     }
